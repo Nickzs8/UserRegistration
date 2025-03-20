e4b5ce6e043dfe98e152eb98eba81238bd194178
Níckolas Davi <nickolasdavi407@gmail.com>
2025-03-19 21:08:03 -0300

    first commit

---
+++ src/test/java/dev/nickzs/CadastroDeUsuario/CadastroDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nickzs.CadastroDeUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
