b947153d151232bbfe18d3e08f6b8ff37082966c
Níckolas Davi <nickolasdavi407@gmail.com>
2025-03-22 11:12:36 -0300

    feat: Further improvement of layered architecture

---
+++ src/main/java/dev/nickzs/CadastroDeUsuario/Assingments/AssingmentRepository.java
@@ -0,0 +1,7 @@
+package dev.nickzs.CadastroDeUsuario.Assingments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssingmentRepository extends JpaRepository<AssingmentModel, Long> {
+
+}
---
+++ src/main/java/dev/nickzs/CadastroDeUsuario/Users/UserService.java
@@ -0,0 +1,4 @@
+package dev.nickzs.CadastroDeUsuario.Users;
+
+public class UserService {
+}
